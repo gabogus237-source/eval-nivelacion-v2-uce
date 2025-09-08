7cae0831c157b3b961aa7858cef07971e154d4b2
gabogus237-source <gabogus237@gmail.com>
2025-09-08 01:26:29 -0500

    Update next.config.mjs

--- eval-nivelacion-v2-uce-theme/next.config.mjs
+++ eval-nivelacion-v2-uce-theme/next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      { source: '/evaluacion', destination: '/cerrada.html', permanent: false },
-      { source: '/evaluacion/:path*', destination: '/cerrada.html', permanent: false },
-      // Para cerrar TODO el sitio, descomenta la siguiente línea:
-      // { source: '/', destination: '/cerrada.html', permanent: false },
-    ];
-  },
-};
-
+const nextConfig = {};
 export default nextConfig;
+
